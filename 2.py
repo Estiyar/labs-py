ae61c5db40aac2b7fd1d8f2e0faa59329e36ea0c
Estiyar <estiartrarbek@gmail.com>
2025-09-25 22:11:23 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,102 @@
+# Booleans
+# x = True
+# y = False
+# print(type(x))
+# print(type(y))
+
+# Operators
+# x = 5
+# y = 2
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+# Lists
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[0])
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+#   print(thislist)
+ 
+
+
+
+
+# Tuples
+# fruits = ("apple", "banana", "cherry")
+# print(fruits[0])
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-1])
+
+# Sets
+# fruits = {"apple", "banana", "cherry"}
+# print("banana" in fruits)
+
+# Dictionaries
+# person = {"name": "John", "age": 36}
+# print(person["name"])
+
+# If...Else
+# x = 20
+# if x > 18:
+#     print("Adult")
+# else:
+#     print("Not an adult")
+
+# Match
+# day = 4
+# match day:
+#    print("Monday")
+#  case 2:
+#    print("Tuesday")
+#  case 3:
+#    print("Wednesday")
+#  case 4:
+#    print("Thursday")
+#  case 5:
+#    print("Friday")
+#  case 6:
+#    print("Saturday")
+#  case 7:
+#    print("Sunday")
+
+# While Loops
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+# i = 1
+# while i < 6:
+#   if i == 3:
+#     break
+#   i += 1
+
+# For Loops
+# for x in range(6):
+#     print(x)
+
+# for x in "banana":
+#  print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+# print(x)
+#   if x == "banana":
+#     break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
