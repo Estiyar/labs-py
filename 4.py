4a289efd7e5b53f0d777a7c8b06e2a51b2e7e3db
Estiyar <estiartrarbek@gmail.com>
2025-10-10 00:43:05 +0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -9,37 +9,41 @@ for square in generate_squares(5):
 Q2
 def even_numbers(n):
     for i in range(0, n + 1):
-        if i % 2 == 0:
-            yield i
+        if i % 2 == 0:       
+            yield i         
 
-n = int(input("Enter n: "))
-print(", ".join(map(str, even_numbers(n))))
+n = int(input("Введите n: "))
 
+for number in even_numbers(n):   
+    print(number, end=" ")
 Q3
-def divisible_by_3_and_4(n):
-    for i in range(n + 1):
+def divisible(n):
+    for i in range(n,n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i
-
-for num in divisible_by_3_and_4(50):
+n = int(input())
+for num in divisible(n):
     print(num)
 
 Q4
-def squares(a, b):
-    for i in range(a, b + 1):
-        yield i ** 2
+def square_num (a,b):
+    for i in range(a,b+1):
+        yield i ** 2 
+a = int(input("напиши начало"))
+b = int(input("напиши конец"))
+for number in square_num (a,b):
+    print (number,end=" " )
 
-for value in squares(3, 7):
-    print(value)
+
 
 Q5
-def countdown(n):
-    while n >= 0:
-        yield n
-        n -= 1
+def dawn(n):
+    for i in range (n, -1,-1):
+        yield i 
+n = int(input("пиши сюда"))
+for numb in dawn(n):
+   print(numb,end=" ")
 
-for num in countdown(5):
-    print(num)
 
 
 Q6
